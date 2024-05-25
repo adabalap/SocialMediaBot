ec20eb3de004882fa2c3a198136234245cda7de6
Phani Adabala <38834236+adabalap@users.noreply.github.com>
2024-05-25 16:57:55 +0530

    Update wordpress_handler.py
    Added variable wp_category_id.

--- wordpress_handler.py
+++ wordpress_handler.py
@@ -12,13 +12,13 @@ def create_wp_post(config, quote, character, quote_prefix, quote_suffix):
     try:
         wp_url = config["wp_url"]
         wp_token = config["wp_token"]  # This should be your JWT token
-        wp_category = config["wp_category"]
+        wp_category_id = config["wp_category_id"]  # This should be your category ID
 
         post = {
             "title": wp_update[:40] + "...",  # Use the first 40 characters of the update as the post title
             "content": wp_update,
             "status": "publish",
-            "categories": [wp_category]  # The category of the post
+            "categories": [wp_category_id]  # The category ID of the post
         }
 
         response = requests.post(f"{wp_url}/wp-json/wp/v2/posts", headers={"Authorization": f"Bearer {wp_token}"}, json=post)
